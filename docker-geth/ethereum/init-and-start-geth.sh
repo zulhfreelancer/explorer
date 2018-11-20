67ca6ea37695b8fabd5a68ebe6983dac21d55de2
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-11-20 08:06:42 +0800

    Improve Geth initialization bash script

--- docker-geth/ethereum/init-and-start-geth.sh
+++ docker-geth/ethereum/init-and-start-geth.sh
@@ -3,6 +3,14 @@
 # Init
 geth --datadir . --syncmode fast init genesis.json
 
+# Move `static-nodes.json` file into `geth` folder
+if [ -e static-nodes.json ]
+then
+  echo "File static-nodes.json found, moving it into 'geth' folder now"
+  mv static-nodes.json geth
+  echo "File moved"
+fi
+
 # CORS - allow explorer on 8002 to talk to Geth on 8545.
 # The `EXTERNAL_IP` here is passed from `docker-compose.yml` file.
 full_url="http://$EXTERNAL_IP:8002"
